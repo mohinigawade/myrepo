33b9640dba5c3e3a8f4c68d018c01eef59518cf2
mohinigawade <mohinigawade89@gmail.com>
2019-10-02 19:52:47 +0530

    Add files via upload

---
+++ Inheritance/Q6/Header.h
@@ -0,0 +1,18 @@
+class base1
+{
+	int num;
+public:
+	base1();
+	base1(int);
+	void disp1();
+	void disp2();
+};
+class base2
+{
+	int num;
+public:
+	base2();
+	base2(int);
+	void disp11();
+	void disp2();
+};
